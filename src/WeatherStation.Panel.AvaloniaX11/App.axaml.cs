8cad4d38ce2539cbd04277086cad9a2dff43f0ec
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2021-08-05 03:48:35 +0300

    Fixed issue with margins and fullscreen mode in Xorg

--- src/WeatherStation.Panel.AvaloniaX11/App.axaml.cs
+++ src/WeatherStation.Panel.AvaloniaX11/App.axaml.cs
@@ -25,6 +25,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace WeatherStation.Panel.AvaloniaX11
 {
@@ -40,13 +41,46 @@ namespace WeatherStation.Panel.AvaloniaX11
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
-                //Full Screen
-#if RELEASE                                
+#if RELEASE
+                //Set start position
+                desktop.MainWindow.Position = new PixelPoint(0, 0);                
+#endif
+                //Set fullscreen
+                try
+                {
+                    var size = GetDisplaySize();
+                    desktop.MainWindow.Width = size.Width;
+                    desktop.MainWindow.Height = size.Height;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при вызове утилиты xrandr - {ex.Message}. {ex}." +
+                        $" Если возникает в образе с Xfce4, то можно игнорировать.");
+                }
+#if RELEASE
+                //For xfce4
                 desktop.MainWindow.WindowState = WindowState.FullScreen;
                 desktop.MainWindow.Topmost = true;
 #endif
             }
             base.OnFrameworkInitializationCompleted();
-        }        
+        }
+        public Size GetDisplaySize()
+        {
+            // Use xrandr to get size of screen located at offset (0,0).
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "xrandr";
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            var match = System.Text.RegularExpressions.Regex.Match(output, @"(\d+)x(\d+)\+0\+0");
+            var w = match.Groups[1].Value;
+            var h = match.Groups[2].Value;
+            Size r = new Size(int.Parse(w), int.Parse(h));
+            Console.WriteLine("Display Size is {0} x {1}", w, h);
+            return r;
+        }
     }
 }
